b4beed4819ba352fab248ab0bfc3fc530e4ed57f
admin <dmitry.dima-sol@yandex.ru>
2024-06-02 15:25:34 +0300

    Complete app without database sad(

--- app/src/main/java/com/example/todo_app/MainActivity.kt
+++ app/src/main/java/com/example/todo_app/MainActivity.kt
@@ -10,16 +10,18 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.ViewModelProvider
 import com.example.todo_app.ui.theme.ToDo_AppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val todoViewModel = ViewModelProvider(this)[TodoViewModel::class.java]
         setContent {
             ToDo_AppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    TodoListPage()
+                    TodoListPage(todoViewModel)
                 }
             }
         }
